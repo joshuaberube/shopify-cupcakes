a2d0baf657f6ae63a0e10bf40827669c1ead1b34
Jabinator1 <jabinator1@gmail.com>
2021-11-21 13:40:46 -0700

    testing out more security headers

--- next.config.js
+++ next.config.js
@@ -9,11 +9,29 @@ const securityHeaders = [
     key: 'Referrer-Policy',
     value: 'strict-origin-when-cross-origin'
   },
-  // {
-  //   key: 'Content-Security-Policy',
-  //   value: `default-src 'self'; base-uri 'self'; img-src 'self' data: https://cdn.shopify.com; ${isProd ? '' : "script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline';"}`
-  // }
+  {
+    key: 'X-XSS-Protection',
+    value: '0'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+  {
+    key: 'Expect-CT',
+    value: 'max-age=86400, enforce'
+  },
+  {
+    key: 'Content-Security-Policy',
+    value: `default-src 'self'; base-uri 'self'; img-src 'self' data: https://cdn.shopify.com; frame-ancestors 'none'; frame-source 'none'; ${isProd ? '' : "script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline';"}`
+  },
+  {
+    key: 'Feature-Policy',
+    value: `camera 'none'; microphone 'none'`
+  }
 ]
+
+//when a user logs out, use Clear-Site-Data header (somehow, look more into it)
 
 module.exports = {
   reactStrictMode: true,
--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,33 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import crypto from 'crypto'
-import { v4 } from 'uuid'
+// import crypto from 'crypto'
+// import { v4 } from 'uuid'
 
-const generateCsp = () => {
-  const isProd = process.env.NODE_ENV === 'production'
+// const generateCsp = () => {
+//   const isProd = process.env.NODE_ENV === 'production'
 
-  const hash = crypto.createHash('sha256')
-  hash.update(v4())
-  const nonce = hash.digest('base64')
+//   const hash = crypto.createHash('sha256')
+//   hash.update(v4())
+//   const nonce = hash.digest('base64')
 
-  const csp = `default-src 'self'; base-uri 'self'; img-src 'self' data: https://cdn.shopify.com; ${isProd ? '' : "script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline';"}`
+//   const csp = `default-src 'self'; base-uri 'self'; img-src 'self' data: https://cdn.shopify.com; ${isProd ? '' : "script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline';"}`
 
-  return { csp, nonce }
-}
+//   return { csp, nonce }
+// }
 
 class MyDocument extends Document {
   render() {
-    const { csp, nonce } = generateCsp()
+    // const { csp, nonce } = generateCsp()
 
     return (
       <Html lang="en">
-        <Head nonce={nonce}>
+        {/* <Head nonce={nonce}>
           <meta property="csp-nonce" content={nonce} />
           <meta httpEquiv="Content-Security-Policy" content={csp} />
-        </Head>
+        </Head> */}
+        <Head />
         <body>
           <Main />
-          <NextScript nonce={nonce} />
+          <NextScript />
         </body>
       </Html>
     )
